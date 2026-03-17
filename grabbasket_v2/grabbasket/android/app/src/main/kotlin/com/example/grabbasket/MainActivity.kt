a196e1df565956f053212b4ff71d3c3a8ad257b0
harishaji <h.krishnan@ai-build.com>
2026-03-18 02:40:56 +0530

    cleaning up

--- grabbasket_v2/grabbasket/android/app/src/main/kotlin/com/example/grabbasket/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.grabbasket
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- v1/tmp_print_lines.php
+++
@@ -1,7 +0,0 @@
-<?php
-$path = __DIR__ . '/app/Support/number_polyfill.php';
-$lines = file($path);
-foreach ($lines as $i => $l) {
-    printf("%4d: %s", $i+1, rtrim($l));
-    echo PHP_EOL;
-}
